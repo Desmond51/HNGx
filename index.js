0ad7a89f098e23948a8a0f126159bc000802a9ce
Desmond51 <desmondtekum9@gmail.com>
2023-09-10 22:44:51 +0100

    Adding time

--- index.js
+++ index.js
@@ -1,4 +1,47 @@
 $(document).ready(function(){
+    //   // Current date
+    //   let today= new Date();
+    //   let date = today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear();
+    //   document.getElementById("current-date").innerHTML = date;
+    //   // current time 
+    //   let time = today.getHours() + ':' + today.getMinutes()+ ':' + today.getSeconds() +':' + today.getMilliseconds();
+    //   document.getElementById("current-time").innerHTML = time
+    let today = new Date();
+    let day = today.getDay();
+    let dayList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    // document.getElementById(".day").innerHTML = "Today is :" + dayList[day] + ".";
+
+    let hour = today.getHours();
+    let minute = today.getMinutes();
+    let seconds = today.getSeconds()
+    let milliseconds = today.getMilliseconds();
+
+    let prepand = (hour >= 12) ? "PM" : "AM";
+    hour = (hour >=12) ? hour - 12 : hour;
+
+    if(hour ===0 && prepand === "PM"){
+        if(minute == minute===0 && seconds === 0){
+            hour= 12;
+            prepand= "Noon";
+        }
+        else{
+            hour =12;
+            prepand = "PM";
+        }
+    }
+    if(hour ===0 && prepand === "AM"){
+        if(minute ===0 && seconds === 0){
+            hour= 12;
+            prepand= "Midnight";
+        }
+        else{
+            hour =12;
+            prepand = "AM";
+        }
+    }
+    document.getElementById(".time").innerHTML = "Current time is:" + ':' + hour + prepand + ':' + minute +':' + seconds;
+// console.log("Current time is :" + hour + prepand + ":" + minute + ":" + seconds)
+      
     $(window).scroll(function(){
         // sticky navbar on scroll script
         if(this.scrollY > 20){
@@ -14,6 +57,7 @@ $(document).ready(function(){
             $('.scroll-up-btn').removeClass("show");
         }
     });
+  
 
     // slide-up script
     $('.scroll-up-btn').click(function(){
